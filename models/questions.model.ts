517b219810e3981c21065208d21512eddb870118
Cristian Ordonez <cristianordonezrd@gmail.com>
2022-06-22 19:37:02 -0400

    refactored request to include answers

--- models/questions.model.ts
+++ models/questions.model.ts
@@ -8,7 +8,10 @@ interface QuestionQuery {
 
 const get = (query: QuestionQuery) => {
    let product_id: number = parseInt(query.product_id);
-   let dbQuery = `SELECT * FROM questions WHERE product_id=${product_id}`;
+   // let dbQuery = `SELECT * FROM questions WHERE product_id=${product_id}`;
+   let dbQuery = `SELECT * FROM questions LEFT JOIN answers ON (questions.id = answers.question_id) WHERE questions.product_id=${product_id}`;
+
+   let secondQuery = `SELECT * from answers where answers.question_id=questions.id`;
    let results = pool.query(dbQuery);
    return results;
 };
--- routes/questions.route.ts
+++ routes/questions.route.ts
@@ -3,7 +3,6 @@ import * as controller from '../controllers/questions.controller';
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-   console.log('req in questions route:', req);
    controller.getQuestions(req, res);
 });
 
